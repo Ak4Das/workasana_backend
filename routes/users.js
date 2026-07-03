a9e28e476d0a237947bfd95ec7dd435d13b0930d
Akash Das <akashdas02052@gmail.com>
2026-07-03 14:20:50 +0530

    Create patch profile route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
 const auth = require("../middleware/auth")
+const bcrypt = require("bcryptjs/dist/bcrypt")
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -18,6 +19,43 @@ router.get("/", auth, async (req, res) => {
       success: false,
       message: error.message,
     })
+  }
+})
+
+router.patch("/profile", auth, async (req, res) => {
+  try {
+    const userProfile = await User.findById(req.user.id)
+    if (!userProfile)
+      return res.status(400).json({ error: "User profile not found." })
+
+    if (req.body.name) {
+      userProfile.name = req.body.name.trim()
+    }
+
+    if (req.body.newPassword) {
+      const matchesCurrent = await bcrypt.compare(
+        req.body.currentPassword,
+        userProfile.password,
+      )
+      if (!matchesCurrent) {
+        return res.status(400).json({
+          error: "The provided active validation password is invalid.",
+        })
+      }
+
+      userProfile.password = req.body.newPassword
+    }
+
+    const response = await userProfile.save()
+    res.status(200)
+    res.json({
+      success: true,
+      message: "Settings saved successfully.",
+      respondedData: response,
+    })
+  } catch (error) {
+    console.dir(error)
+    res.status(500).json({ success: false, message: error.message })
   }
 })
 
