17bed2e8c3c1b2c4db8b0a1079fffa5f2e0edf47
Akash Das <akashdas02052@gmail.com>
2026-07-03 19:53:12 +0530

    Add comments to User schema

--- models/User.js
+++ models/User.js
@@ -17,9 +17,12 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true },
 )
 
+// Register middleware that executes before "save" operation
 UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next()
-  const salt = await bcrypt.genSalt(10)
+  if (!this.isModified("password")) { // "this" points to document being saved
+    return next()
+  }
+  const salt = await bcrypt.genSalt(10) // A salt is random data added to a password before hashing (Here 10 is the cost factor)
   this.password = await bcrypt.hash(this.password, salt)
   next()
 })
